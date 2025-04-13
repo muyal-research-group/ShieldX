6bb3dc3a3ee32b98f4fd80a48cfe27891ecae0cc
Edgar2607821 <edgarballeza87@gmail.com>
2025-04-13 13:04:00 -0600

    refactor(server): include new routers with tags and prefixes

--- shieldx/server.py
+++ shieldx/server.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
-from shieldx.controllers import events_router, trigger_router
+import shieldx.controllers as Controllers
 from shieldx.db import connect_to_mongo,close_mongo_connection, get_database
 import uvicorn
 import asyncio
 from contextlib import asynccontextmanager
 from shieldx.db.indexes import create_indexes
-
 
 
 @asynccontextmanager
@@ -27,17 +26,32 @@ async def lifespan(app: FastAPI):
     await close_mongo_connection()
 
 
-app = FastAPI(title="ShieldX API",lifespan=lifespan, description="API para registrar y consultar eventos generados por microservicios.",
+app = FastAPI(
+    title="ShieldX API",
+    lifespan=lifespan, 
+    description="API para registrar y consultar eventos generados por microservicios.",
     contact={
         "name": "Equipo ShieldX",
         "email": "soporte@shieldx.io",
     })
 
+
 # Include API routes from the service controller under /api/v1
-# Rutas de eventos
-app.include_router(events_router, prefix="/api/v1",tags=["Eventos"],)
-# Rutas de triggers
-app.include_router(trigger_router, prefix="/api/v1", tags=["Triggers"])
+# Rutas para la gestión de eventos generados por servicios
+app.include_router(Controllers.events_router, prefix="/api/v1",tags=["Eventos"],)
+# Rutas para la gestión de triggers del sistema
+app.include_router(Controllers.trigger_router, prefix="/api/v1", tags=["Triggers"])
+# Rutas para la gestión de tipos de eventos
+app.include_router(Controllers.event_types_router, prefix="/api/v1", tags=["Tipos de Evento"])
+# Rutas para administrar la relación EventType <-> Trigger
+app.include_router(Controllers.events_triggers_router, prefix="/api/v1", tags=["Event-Triggers"])
+# Rutas para manejar la relación jerárquica entre triggers (parent-child)
+app.include_router(Controllers.triggers_triggers_router, prefix="/api/v1", tags=["Triggers-Triggers"])
+# Rutas para administrar la relación Trigger <-> Rule
+app.include_router(Controllers.rules_trigger_router, prefix="/api/v1", tags=["Trigger - Rule"])
+# Rutas para CRUD de reglas
+app.include_router(Controllers.rules_router, prefix="/api/v1",  tags=["Rules"])
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=20000)
